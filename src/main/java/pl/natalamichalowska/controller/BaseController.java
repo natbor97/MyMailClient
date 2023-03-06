9894993de66488356c27aa94fd42069f87f258f1
Natalia Borkowska <natalia.borkowska.programista@gmail.com>
2023-03-06 15:50:27 +0100

    Initializing stages

--- src/main/java/pl/natalamichalowska/controller/BaseController.java
+++ src/main/java/pl/natalamichalowska/controller/BaseController.java
@@ -6,11 +6,15 @@ import pl.natalamichalowska.view.ViewFactory;
 public abstract class BaseController {
     private EmailManager emailManager;
     private ViewFactory viewFactory;
-    private String fxmlName;
+    String fxmlName;
 
     public BaseController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         this.emailManager = emailManager;
         this.viewFactory = viewFactory;
         this.fxmlName = fxmlName;
     }
+
+    public String getFxmlName() {
+        return fxmlName;
+    };
 }
--- src/main/java/pl/natalamichalowska/view/ViewFactory.java
+++ src/main/java/pl/natalamichalowska/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package pl.natalamichalowska.view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import pl.natalamichalowska.EmailManager;
+import pl.natalamichalowska.controller.BaseController;
+import pl.natalamichalowska.controller.LoginWindowController;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -9,6 +17,22 @@ public class ViewFactory {
         this.emailManager = emailManager;
     }
     public void showLoginWindow(){
-        System.out.println("show login window called");
+        System.out.println("Login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "/LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
