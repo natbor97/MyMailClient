d322334d3d78cea68c9b3883a3769fe96ff24dab
Natalia Borkowska <natalia.borkowska.programista@gmail.com>
2023-03-06 15:23:14 +0100

    base controller

--- src/main/java/pl/natalamichalowska/Launcher.java
+++ src/main/java/pl/natalamichalowska/Launcher.java
@@ -4,16 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.natalamichalowska.view.ViewFactory;
 
 import java.io.IOException;
 
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+        ViewFactory viewFactory = new ViewFactory(new EmailManager());
+        viewFactory.showLoginWindow();
     }
 
     public static void main(String[] args) {
