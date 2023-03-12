fdabb1e4f0de7d4cf55cc6e430740532967d5614
Natalia Borkowska <natalia.borkowska.programista@gmail.com>
2023-03-12 16:15:55 +0100

    Email actions

--- src/main/java/pl/natalamichalowska/EmailManager.java
+++ src/main/java/pl/natalamichalowska/EmailManager.java
@@ -4,19 +4,26 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import pl.natalamichalowska.controller.services.FetchFoldersService;
+import pl.natalamichalowska.controller.services.FolderUpdaterService;
 import pl.natalamichalowska.model.EmailAccount;
 import pl.natalamichalowska.model.EmailMessage;
 import pl.natalamichalowska.model.EmailTreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+    private FolderUpdaterService folderUpdaterService;
     private EmailMessage selectedMessage;
     private EmailTreeItem<String> selectedFolder;
-    private ObservableList<EmailAccount> emailAccounts = FXCollections.observableArrayList();
 
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+    private ObservableList<EmailAccount> emailAccounts = FXCollections.observableArrayList();
     public  ObservableList<EmailAccount> getEmailAccounts(){
         return  emailAccounts;
     }
@@ -51,11 +58,41 @@ public class EmailManager {
     }
 
     public void addEmailAccount(EmailAccount emailAccount){
-        emailAccounts.add(emailAccount);
+        //emailAccounts.add(emailAccount);
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
         //treeItem.setGraphic(iconResolver.getIconForFolder(emailAccount.getAddress()));
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setUnRead() {
+        try {
+            selectedMessage.setRead(false);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, false);
+            selectedFolder.incrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteSelectedMessage() {
+        try {
+            selectedMessage.getMessage().setFlag(Flags.Flag.DELETED, true);
+            selectedFolder.getEmailMessages().remove(selectedMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
