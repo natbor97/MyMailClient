8f79f34ead6f90c6b28d386b5026b3ba1e7fd6f3
natbor97 <99174052+natbor97@users.noreply.github.com>
2023-03-17 18:18:55 +0100

    Update LoginWindowController.java

--- src/main/java/pl/natalamichalowska/controller/LoginWindowController.java
+++ src/main/java/pl/natalamichalowska/controller/LoginWindowController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package pl.natalamichalowska.controller;
 
 import javafx.fxml.FXML;
@@ -31,134 +31,50 @@ public class LoginWindowController extends BaseController implements Initializab
 
     @FXML
     void loginButtonAction() {
-        System.out.println("loginButtonAction");
-        if (fieldsAreValid()){
+        System.out.println("loginButtonAction!!");
+        if(fieldsAreValid()){
             EmailAccount emailAccount = new EmailAccount(emailAddress.getText(), passwordField.getText());
             LoginService loginService = new LoginService(emailAccount, emailManager);
             loginService.start();
             loginService.setOnSucceeded(event -> {
-                EmailLoginResult emailLoginResult = loginService.getValue();
-
+                EmailLoginResult emailLoginResult= loginService.getValue();
                 switch (emailLoginResult) {
                     case SUCCESS:
-                        System.out.println("login success: "+emailAccount);
-                        viewFactory.showMainWindow();
+                        System.out.println("login succesfull!!!" + emailAccount);
+                        if(!viewFactory.isMainViewInitialized()){
+                            viewFactory.showMainWindow();
+                        }
                         Stage stage = (Stage) errorLabel.getScene().getWindow();
                         viewFactory.closeStage(stage);
-                        System.out.println("loginWindowClosed");
                         return;
                     case FAILED_BY_CREDENTIALS:
-                        errorLabel.setText("Invalid credentials");
-                        return;
-                    case FAILED_BY_NETWORK:
-                        errorLabel.setText("Connection error");
+                        errorLabel.setText("invalid credentials!");
                         return;
                     case FAILED_BY_UNEXPECTED_ERROR:
-                        errorLabel.setText("something gone wrong");
+                        errorLabel.setText("unexpected error!");
                         return;
-                    default: return;
+                    default:
+                        return;
                 }
             });
         }
     }
 
     private boolean fieldsAreValid() {
-        if (emailAddress.getText().isEmpty()){
+        if(emailAddress.getText().isEmpty()) {
             errorLabel.setText("Please fill email");
             return false;
         }
-        if (passwordField.getText().isEmpty()){
+        if(passwordField.getText().isEmpty()) {
             errorLabel.setText("Please fill password");
             return false;
         }
-        else return true;
+        return true;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        emailAddress.setText("natalia.michalowska97@op.pl");
+        emailAddress.setText("natalia.michalowska97@wp.pl");
         passwordField.setText("haslo");
     }
-=======
-package pl.natalamichalowska.controller;
-
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import pl.natalamichalowska.EmailManager;
-import pl.natalamichalowska.controller.services.LoginService;
-import pl.natalamichalowska.model.EmailAccount;
-import pl.natalamichalowska.view.ViewFactory;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class LoginWindowController extends BaseController implements Initializable {
-    @FXML
-    private TextField emailAddress;
-
-    @FXML
-    private Label errorLabel;
-
-    @FXML
-    private PasswordField passwordField;
-
-    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
-        super(emailManager, viewFactory, fxmlName);
-    }
-
-    @FXML
-    void loginButtonAction() {
-        System.out.println("loginButtonAction");
-        if (fieldsAreValid()){
-            EmailAccount emailAccount = new EmailAccount(emailAddress.getText(), passwordField.getText());
-            LoginService loginService = new LoginService(emailAccount, emailManager);
-            loginService.start();
-            loginService.setOnSucceeded(event -> {
-                EmailLoginResult emailLoginResult = loginService.getValue();
-
-                switch (emailLoginResult) {
-                    case SUCCESS:
-                        System.out.println("login success: "+emailAccount);
-                        viewFactory.showMainWindow();
-                        Stage stage = (Stage) errorLabel.getScene().getWindow();
-                        viewFactory.closeStage(stage);
-                        System.out.println("loginWindowClosed");
-                        return;
-                    case FAILED_BY_CREDENTIALS:
-                        errorLabel.setText("Invalid credentials");
-                        return;
-                    case FAILED_BY_NETWORK:
-                        errorLabel.setText("Connection error");
-                        return;
-                    case FAILED_BY_UNEXPECTED_ERROR:
-                        errorLabel.setText("something gone wrong");
-                        return;
-                    default: return;
-                }
-            });
-        }
-    }
-
-    private boolean fieldsAreValid() {
-        if (emailAddress.getText().isEmpty()){
-            errorLabel.setText("Please fill email");
-            return false;
-        }
-        if (passwordField.getText().isEmpty()){
-            errorLabel.setText("Please fill password");
-            return false;
-        }
-        else return true;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        emailAddress.setText("natalia.michalowska97@op.pl");
-        passwordField.setText("haslo");
-    }
->>>>>>> 7b8aa44dbf64e1ca61ffe1322b704fbce41d8ef2
 }
