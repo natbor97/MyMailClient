fc211ec02694a6870d8ec624ee2aab4e87ad3f84
natbor97 <99174052+natbor97@users.noreply.github.com>
2023-03-17 18:10:33 +0100

    Update FolderUpdaterService.java

--- src/main/java/pl/natalamichalowska/controller/services/FolderUpdaterService.java
+++ src/main/java/pl/natalamichalowska/controller/services/FolderUpdaterService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package pl.natalamichalowska.controller.services;
 
 import javafx.concurrent.Service;
@@ -37,43 +36,3 @@ public class FolderUpdaterService extends Service {
         };
     }
 }
-=======
-package pl.natalamichalowska.controller.services;
-
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-
-import javax.mail.Folder;
-import java.util.List;
-
-public class FolderUpdaterService extends Service {
-
-    private List<Folder> folderList;
-
-    public FolderUpdaterService(List<Folder> folderList) {
-        this.folderList = folderList;
-    }
-
-
-    @Override
-    protected Task createTask() {
-        return new Task() {
-            @Override
-            protected Object call() throws Exception {
-                for (;;){
-                    try {
-                        Thread.sleep(5000);
-                        for (Folder folder: folderList){
-                            if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen()){
-                                folder.getMessageCount();
-                            }
-                        }
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-    }
-}
->>>>>>> 7b8aa44dbf64e1ca61ffe1322b704fbce41d8ef2
