bd1bdbda6bfd0f30c5f1e20841950ecb4760446f
natbor97 <99174052+natbor97@users.noreply.github.com>
2023-03-17 18:22:59 +0100

    Update EmailTreeItem.java

--- src/main/java/pl/natalamichalowska/model/EmailTreeItem.java
+++ src/main/java/pl/natalamichalowska/model/EmailTreeItem.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-package pl.natalamichalowska.model;
+package pl.nataliamichalowska.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,13 +17,12 @@ public class EmailTreeItem<String> extends TreeItem<String> {
     public EmailTreeItem(String name) {
         super(name);
         this.name = name;
-        this.emailMessages= FXCollections.observableArrayList();
+        this.emailMessages = FXCollections.observableArrayList();
     }
 
     public ObservableList<EmailMessage> getEmailMessages(){
-        return emailMessages;
+        return  emailMessages;
     }
-
 
     public void addEmail(Message message) throws MessagingException {
         EmailMessage emailMessage = fetchMessage(message);
@@ -62,86 +60,11 @@ public class EmailTreeItem<String> extends TreeItem<String> {
         updateName();
     }
 
-    private void updateName() {
-        if (unreadMessagesCount > 0) {
-            this.setValue((String) (name + "(" + unreadMessagesCount + ")"));
+    private void updateName(){
+        if(unreadMessagesCount > 0){
+            this.setValue((String)(name + "(" + unreadMessagesCount + ")"));
         } else {
             this.setValue(name);
         }
     }
-
 }
-=======
-package pl.natalamichalowska.model;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.TreeItem;
-
-import javax.mail.Flags;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-public class EmailTreeItem<String> extends TreeItem<String> {
-    private String name;
-    private ObservableList<EmailMessage> emailMessages;
-    private int unreadMessagesCount;
-
-    public EmailTreeItem(String name) {
-        super(name);
-        this.name = name;
-        this.emailMessages= FXCollections.observableArrayList();
-    }
-
-    public ObservableList<EmailMessage> getEmailMessages(){
-        return emailMessages;
-    }
-
-
-    public void addEmail(Message message) throws MessagingException {
-        EmailMessage emailMessage = fetchMessage(message);
-        emailMessages.add(emailMessage);
-    }
-    public void addEmailToTop(Message message) throws MessagingException {
-        EmailMessage emailMessage = fetchMessage(message);
-        emailMessages.add(0, emailMessage);
-    }
-
-    private EmailMessage fetchMessage(Message message) throws MessagingException {
-        boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
-        EmailMessage emailMessage = new EmailMessage(
-                message.getSubject(),
-                message.getFrom()[0].toString(),
-                message.getRecipients(MimeMessage.RecipientType.TO)[0].toString(),
-                message.getSize(),
-                message.getSentDate(),
-                messageIsRead,
-                message
-        );
-        if(!messageIsRead){
-            incrementMessagesCount();
-        }
-        return  emailMessage;
-    }
-
-    public void incrementMessagesCount(){
-        unreadMessagesCount++;
-        updateName();
-    }
-
-    public void decrementMessagesCount(){
-        unreadMessagesCount--;
-        updateName();
-    }
-
-    private void updateName() {
-        if (unreadMessagesCount > 0) {
-            this.setValue((String) (name + "(" + unreadMessagesCount + ")"));
-        } else {
-            this.setValue(name);
-        }
-    }
-
-}
->>>>>>> 7b8aa44dbf64e1ca61ffe1322b704fbce41d8ef2
