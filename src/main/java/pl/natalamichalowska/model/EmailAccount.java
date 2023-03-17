e6c13828875fb7482947a726c6c0143161830fcc
natbor97 <99174052+natbor97@users.noreply.github.com>
2023-03-17 18:21:57 +0100

    Update EmailAccount.java

--- src/main/java/pl/natalamichalowska/model/EmailAccount.java
+++ src/main/java/pl/natalamichalowska/model/EmailAccount.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package pl.natalamichalowska.model;
 
 import javax.mail.Store;
@@ -10,95 +9,17 @@ public class EmailAccount {
     private String password;
     private Properties properties;
     private Store store;
-    private Session session;
 
     public Session getSession() {
         return session;
     }
+
     public void setSession(Session session) {
         this.session = session;
     }
 
-    public EmailAccount(String address, String password) {
-        this.address = address;
-        this.password = password;
-        properties = new Properties();
-
-        properties.put("incomingHost", "imap.op.pl");
-        properties.put("mail.store.protocol", "imaps");
-
-        properties.put("mail.transport.protocol", "smtps");
-        properties.put("mail.smtps.host", "smtp.op.pl");
-        properties.put("mail.smtps.auth", "true");
-        properties.put("outgoingHost", "smtp.op.pl");
-
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Properties getProperties() {
-        return properties;
-    }
-
-    public void setProperties(Properties properties) {
-        this.properties = properties;
-    }
-
-    public Store getStore() {
-        return store;
-    }
-
-    public void setStore(Store store) {
-        this.store = store;
-    }
-
-    @Override
-    public String toString() {
-        return address;
-    }
-
-
-
-}
-=======
-package pl.natalamichalowska.model;
-
-import javax.mail.Store;
-import javax.mail.Session;
-import java.util.Properties;
-
-public class EmailAccount {
-    private String address;
-    private String password;
-    private Properties properties;
-    private Store store;
     private Session session;
 
-    public void setSession(Session session) {
-        this.session = session;
-    }
-
-    public EmailAccount(String address, String password) {
-        this.address = address;
-        this.password = password;
-        properties = new Properties();
-
-        properties.put("incomingHost", "imap.op.pl");
-        properties.put("mail.store.protocol", "imaps");
-
-        properties.put("mail.transport.protocol", "smtps");
-        properties.put("mail.smtps.host", "smtp.op.pl");
-        properties.put("mail.smtps.auth", "true");
-        properties.put("outgoingHost", "smtp.op.pl");
-
-    }
-
     public String getAddress() {
         return address;
     }
@@ -115,6 +36,11 @@ public class EmailAccount {
         this.properties = properties;
     }
 
+    @Override
+    public String toString() {
+        return address;
+    }
+
     public Store getStore() {
         return store;
     }
@@ -123,11 +49,16 @@ public class EmailAccount {
         this.store = store;
     }
 
-    @Override
-    public String toString() {
-        return address;
+    public EmailAccount(String address, String password) {
+        this.address = address;
+        this.password = password;
+        properties = new Properties();
+        properties.put("incomingHost", "imap.op.pl");
+        properties.put("mail.store.protocol", "imaps");
+
+        properties.put("mail.transport.protocol", "smtps");
+        properties.put("mail.smtps.host", "smtp.op.pl");
+        properties.put("mail.smtps.auth", "true");
+        properties.put("outgoingHost", "smtp.op.pl");
     }
-
-
 }
->>>>>>> 7b8aa44dbf64e1ca61ffe1322b704fbce41d8ef2
